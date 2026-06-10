42584fe54a600f38d3b35b6a7ffc2c5d85bbb6a7
keduaakunraid-byte <keduaakunraid@gmail.com>
2026-06-10 23:36:49 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -31,6 +31,7 @@ OWNER_USERNAME = "@KINGZAAASLI"
 client = MongoClient(MONGO_URI)
 db = client["telegram_bot"]
 groups_col = db["groups"]
+chat_logs = db["chat_losg"]
 #================= RESPONSE =================
 
 RESP = {
